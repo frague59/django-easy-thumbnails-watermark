104765f94b03a623fe29ec4ea5c7b9d5238061bc
GUERIN François <fguerin@ville-tourcoing.fr>
2018-01-29 10:44:03 +0100

    + Update version for setup

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+# Loads the version from package
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+from easy_thumbnails_watermark import __version__ as version
 
 setup(
     name='django-easy-thumbnails-watermark-1',
-    version='0.7.0',
+    version=version,
     packages=['easy_thumbnails_watermark'],
     include_package_data=True,
     license='GPLv3 License',
